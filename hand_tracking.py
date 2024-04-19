5ac0d026ff228a8f861e84b043ae215de5cb0b85
calimanis-ygor <ygoralvescalimanis@hotmail.com>
2024-04-19 00:43:43 -0300

    refatoração do código

--- hand_tracking.py
+++ hand_tracking.py
@@ -1,38 +1,45 @@
 import cv2
 import mediapipe as mp
 
-# acessando soluções do mediapipe
+# Inicialização das soluções do mediapipe para detecção de mãos
 mp_hands = mp.solutions.hands
 mp_draw = mp.solutions.drawing_utils
 
-# adicionando modelos de ML para leitura das mãos
+# Inicialização do modelo de detecção de mãos
 hands = mp_hands.Hands()
 
-# realizando a conexão com a webcam
+# Inicialização da captura de vídeo da webcam
 camera = cv2.VideoCapture(0)
 
-# aumentando a resolução da imagem
+# Configuração da resolução da imagem da câmera
 resolution_x = 1280
 resolution_y = 720
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, resolution_x)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution_y)
 
-# mostrando em tempo real a imagem
+# Loop para mostrar a imagem em tempo real
 while True:
-    # O primeiro deles é o sucesso (true ou false) e o segundo é a imagem da camera
-    sucess, img = camera.read()
+    # Captura de um frame da câmera
+    success, img = camera.read()
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+    # Processamento da imagem para detecção de mãos
     result = hands.process(img_rgb)
 
+    # Verificação se há mãos detectadas na imagem
     if result.multi_hand_landmarks:
+        # Desenho dos pontos e conexões das mãos na imagem
         for hand_marks in result.multi_hand_landmarks:
             mp_draw.draw_landmarks(img, hand_marks, mp_hands.HAND_CONNECTIONS)
+
+    # Exibição da imagem em uma janela chamada "Imagem"
     cv2.imshow("Imagem", img)
 
-    # aguarda um determinado tempo para que o código continue sendo executado
+    # Aguarda um determinado tempo e verifica se a tecla "ESC" foi pressionada para sair do loop
     key = cv2.waitKey(1)
-    # tecal "ESC" é o número 27
-    # A waitKey também aguarda uma tecla do teclado que será utilizada para encerrar o loop
-    if key == 27:
+    if key == 27:  # "ESC" é o número 27 na tabela ASCII
         break
+
+# Liberação dos recursos utilizados
+camera.release()
+cv2.destroyAllWindows()
