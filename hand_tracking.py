6774a0f8c2dfcdd1502dd8f6dfc4eb1ab6828571
calimanis-ygor <ygoralvescalimanis@hotmail.com>
2024-04-21 23:06:14 -0300

    abrindo e fechando arquivos

--- hand_tracking.py
+++ hand_tracking.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import os
 
 # Inicialização das soluções do mediapipe para detecção de mãos
 mp_hands = mp.solutions.hands
@@ -16,25 +17,41 @@ resolution_x = 1280
 resolution_y = 720
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, resolution_x)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution_y)
+notepad = False
+chrome = False
+calculadora = False
 
 
-def find_hands_coordinates(img, side_inverted = False):
+def find_hands_coordinates(img, side_inverted=False):
+    """
+    Encontra e desenha as coordenadas das mãos na imagem.
+
+    Args:
+        img: Imagem de entrada.
+        side_inverted: Indica se o lado das mãos está invertido.
+
+    Returns:
+        img: Imagem com as coordenadas das mãos desenhadas.
+        all_hands: Lista de informações das mãos encontradas.
+    """
     # Converte a imagem para RGB (mediapipe requer RGB)
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     # Processamento da imagem para detecção de mãos
     result = hands.process(img_rgb)
 
-    all_hands = [] # dicionário que irá armazenar as informações de todas as mãos
+    all_hands = []  # Lista que armazenará informações de todas as mãos
 
     # Verifica se há mãos detectadas na imagem
     if result.multi_hand_landmarks:
         # Desenha os pontos e conexões das mãos na imagem
-        for side_hand, hand_marks in zip(result.multi_handedness, result.multi_hand_landmarks):
+        for side_hand, hand_marks in zip(
+            result.multi_handedness, result.multi_hand_landmarks
+        ):
             # Coleta as coordenadas
             hand_info = {}
             coordinates = []
-            for  mark in hand_marks.landmark:
+            for mark in hand_marks.landmark:
                 coord_x, coord_y, coord_z = (
                     int(mark.x * resolution_x),  # Largura
                     int(mark.y * resolution_y),  # Altura
@@ -42,22 +59,17 @@ def find_hands_coordinates(img, side_inverted = False):
                 )
                 coordinates.append(
                     (coord_x, coord_y, coord_z)
-                )  # Armazena todas as coordenadas da mão em uma lista
-
+                )  # Armazena as coordenadas
             hand_info["coordenadas"] = coordinates
 
-            # checando se o lado está invertido 
+            # Verifica e ajusta o lado da mão conforme necessário
             if side_inverted:
-                # invertendo os valores
-                if side_hand.classification[0].label == 'Left':
-                   hand_info['lado'] = 'Right'
+                if side_hand.classification[0].label == "Left":
+                    hand_info["lado"] = "Right"
                 else:
-                    side_hand.classification[0].label = 'Right'
+                    hand_info["lado"] = "Left"
             else:
-                hand_info['lado'] = side_hand.classification[0].label
-                
-            # acessando informação sobre qual é o lado da mão
-            print(side_hand.classification[0].label)
+                hand_info["lado"] = side_hand.classification[0].label
 
             all_hands.append(hand_info)
 
@@ -67,20 +79,68 @@ def find_hands_coordinates(img, side_inverted = False):
     return img, all_hands
 
 
+def raised_fingers(hand):
+    """
+    Verifica quais dedos estão levantados em uma mão.
+
+    Args:
+        hand: Informações da mão.
+
+    Returns:
+        fingers: Lista de booleanos indicando quais dedos estão levantados.
+    """
+    fingers = []
+
+    for finger_tip in [8, 12, 16, 20]:
+        fingers.append(
+            hand["coordenadas"][finger_tip][1] < hand["coordenadas"][finger_tip - 2][1]
+        )
+
+    return fingers
+
+
 # Loop para mostrar a imagem em tempo real
 while True:
     # Captura de um frame da câmera
     success, img = camera.read()
-    # invertendo a imagem
-    img = cv2.flip(img, 1) # 1 - inverte a esquerda pela direita
+    # Inverte a imagem horizontalmente
+    img = cv2.flip(img, 1)
 
     # Encontra e desenha as coordenadas das mãos na imagem
     img, all_hands = find_hands_coordinates(img)
 
+    if len(all_hands) == 1:
+        info_finger_hand1 = raised_fingers(all_hands[0])
+        # só executa se for a mão direita
+        if all_hands[0]["lado"] == "Right":
+            # abrindo o notepad com o indicador
+            if info_finger_hand1 == [True, False, False, False] and notepad == False:
+                notepad = True
+                os.startfile(r"C:\Windows\notepad.exe")
+
+            # abrindo o chrome com o indicador e o dedo médio
+            if info_finger_hand1 == [True, True, False, False] and chrome == False:
+                chrome = True
+                os.startfile(r"C:\Program Files\Google\Chrome\Application\chrome.exe")
+
+            # abrindo a calculadora com o indicador e o dedo médio e anelar
+            if info_finger_hand1 == [True, True, True, False] and calculadora == False:
+                calculadora = True
+                os.startfile(r"C:\Windows\System32\calc.exe")
+
+            # fechando o bloco de notas quando todos os dedos estiverem abaixados
+            if info_finger_hand1 == [False, False, False, False] and notepad == True:
+                notepad = False
+                os.system("TASKKILL /IM notepad.exe")
+
+            # Encerrando o loop caso o dedo indicador e o minimo estejam levantados
+            if info_finger_hand1 == [True, False, False, True]:
+                break
+
     # Mostra a imagem em uma janela
     cv2.imshow("Imagem", img)
 
-    # Aguarda um determinado tempo e verifica se a tecla "ESC" foi pressionada para sair do loop
+    # Aguarda um determinado tempo e verifica se a tecla 'ESC' foi pressionada para sair do loop
     key = cv2.waitKey(1)
     if key == 27:
         break
